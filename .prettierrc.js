11897d3d83bf2cba956668116347cc999b8cd511
ubmishra200 <ubmishra200@gmail.com>
2024-03-21 19:20:18 +0530

    untrack files add

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+const { SemicolonPreference } = require("typescript");
+
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSameLine: true,
+  bracketSpacing: false,
+  singleQuote: false,
+  trailingComma: 'all',
+  SemicolonPreference:false
+  
+};
